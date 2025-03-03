4adaa816d7555e758d364d3b75dff674df923501
yhqz01 <157672792+YHQZ1@users.noreply.github.com>
2025-03-03 11:58:16 +0530

    Time: 0 ms (100%), Space: 8.9 MB (68.62%) - LeetHub

---
+++ 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.c
@@ -0,0 +1,11 @@
+int findClosestNumber(int* nums, int numsSize) {
+    int closest = nums[0];
+    
+    for (int i = 1; i < numsSize; i++) {
+        if (abs(nums[i]) < abs(closest) || (abs(nums[i]) == abs(closest) && nums[i] > closest)) {
+            closest = nums[i];
+        }
+    }
+    
+    return closest;
+}
