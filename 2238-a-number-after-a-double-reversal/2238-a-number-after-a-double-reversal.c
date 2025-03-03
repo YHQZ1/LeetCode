8cf2b4eda53f5460623628b5e2435b89bd5eb0bc
yhqz01 <157672792+YHQZ1@users.noreply.github.com>
2025-03-03 12:11:36 +0530

    Time: 0 ms (100%), Space: 7.8 MB (39.6%) - LeetHub

---
+++ 2238-a-number-after-a-double-reversal/2238-a-number-after-a-double-reversal.c
@@ -0,0 +1,16 @@
+int reverse(int x) {
+    int rem, rev = 0;
+    while (x != 0) {
+        rem = x % 10;
+        rev = (rev * 10) + rem;
+        x = x / 10;
+    }
+    return rev;
+}
+
+bool isSameAfterReversals(int num) {
+    int a = reverse(num);
+    int b = reverse(a);
+
+    return num == b;  // Directly return the comparison result
+}
