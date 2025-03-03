5ad56ae844b6d03f93ae80b4f55e71c6b3c1b7b5
yhqz01 <157672792+YHQZ1@users.noreply.github.com>
2025-03-03 12:09:36 +0530

    Time: 86 ms (7.4%), Space: 14.7 MB (93.99%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.c
@@ -0,0 +1,52 @@
+void merge(int* nums, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int L[n1], R[n2];
+    for (int i = 0; i < n1; i++)
+        L[i] = nums[left + i];
+    for (int i = 0; i < n2; i++)
+        R[i] = nums[mid + 1 + i];
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            nums[k] = L[i];
+            i++;
+        } else {
+            nums[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        nums[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        nums[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int* nums, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+        merge(nums, left, mid, right);
+    }
+}
+
+bool containsDuplicate(int* nums, int numsSize) {
+    if (numsSize <= 1) {
+        return false;
+    }
+    mergeSort(nums, 0, numsSize - 1);
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] == nums[i + 1]) {
+            return true;
+        }
+    }
+    return false;
+}
