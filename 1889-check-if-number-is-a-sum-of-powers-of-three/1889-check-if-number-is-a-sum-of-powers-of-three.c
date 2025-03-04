d84efb7c2620f4e9f17f76121b1facbb93f76651
yhqz01 <157672792+YHQZ1@users.noreply.github.com>
2025-03-04 10:35:19 +0530

    Time: 0 ms (100%), Space: 7.6 MB (89.19%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.c
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.c
@@ -1,10 +1,5 @@
 bool checkPowersOfThree(int n) {
-    while(n>0){
-        int rem = n%3;
-        if(rem == 2){
-            return false;
-        }
-        n=n/3;
-    }
-    return true;
+    if(n == 1)return true;
+    if(n%3==2)return false;
+    return checkPowersOfThree(n/3);
 }
