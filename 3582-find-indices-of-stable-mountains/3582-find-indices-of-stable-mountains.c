a8c530d295a505b99a4d5108d50a9992c544e443
yhqz01 <157672792+YHQZ1@users.noreply.github.com>
2025-03-04 22:36:31 +0530

    Time: 0 ms (100%), Space: 10.9 MB (93.1%) - LeetHub

---
+++ 3582-find-indices-of-stable-mountains/3582-find-indices-of-stable-mountains.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* stableMountains(int* height, int heightSize, int threshold, int* returnSize) {
+    int index = 0;  
+    int* result = (int*)malloc(heightSize * sizeof(int)); 
+
+    if (result == NULL) {
+        *returnSize = 0; 
+        return NULL;
+    }
+
+    for (int i = 1; i < heightSize; i++) {
+        if (height[i - 1] > threshold) {
+            result[index++] = i;
+        }
+    }
+
+    *returnSize = index; 
+    return result;
+}
