7a30dec44124135c4b79c1181b733ca3452fe544
yhqz01 <157672792+YHQZ1@users.noreply.github.com>
2025-03-07 20:54:57 +0530

    Time: 3 ms (68.67%), Space: 7.7 MB (48.12%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.c
+++ 1013-fibonacci-number/1013-fibonacci-number.c
@@ -1,20 +1,11 @@
-
 int fib(int n){
-    if(n==0){
-        return 0;
-    }
+    int result;
+    if (n==0)
+    return 0;
+    if (n==1)
+    return 1;
+    else
+    result=fib(n-1)+fib(n-2);
+    return result;
 
-    if(n==1){
-        return 1;
-    }
-
-    int a=0, b=1, next;
-
-    for(int i=2; i<=n; i++){
-        next=a+b;
-        a=b;
-        b=next;
-    }
-
-    return b;
 }
