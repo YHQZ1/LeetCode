04e9654d2b88ef4dea4507c6eb8f78d05e89f308
yhqz01 <157672792+YHQZ1@users.noreply.github.com>
2025-03-12 19:43:05 +0530

    Time: 0 ms (100%), Space: 8.3 MB (0%) - LeetHub

--- 1584-average-salary-excluding-the-minimum-and-maximum-salary/1584-average-salary-excluding-the-minimum-and-maximum-salary.c
+++ 1584-average-salary-excluding-the-minimum-and-maximum-salary/1584-average-salary-excluding-the-minimum-and-maximum-salary.c
@@ -1,40 +1,13 @@
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j < high; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return i + 1;
-}
-
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high); 
-
-        quickSort(arr, low, pi - 1); 
-        quickSort(arr, pi + 1, high); 
-    }
-}
-
 double average(int* salary, int salarySize) {
-    quickSort(salary, 0, salarySize-1);
-    double sum=0;
-    for(int i=1; i<salarySize-1; i++){
-        sum = sum+salary[i];
+    int max = salary[0];
+    int min = salary[0];
+    int sum = 0;
+
+    for (int i = 0; i < salarySize; i++) {
+        sum += salary[i];
+        if (salary[i] > max) max = salary[i];
+        if (salary[i] < min) min = salary[i];
     }
 
-    double avg = sum/(salarySize-2);
-
-    return avg;
+    return (sum - max - min) / (double)(salarySize - 2);
 }
