c26ab1fbd0f09fce4f330570cd578f65a657d2af
yhqz01 <157672792+YHQZ1@users.noreply.github.com>
2025-03-18 18:46:42 +0530

    Time: 0 ms (100%), Space: 8 MB (11.33%) - LeetHub

---
+++ 2575-minimum-cuts-to-divide-a-circle/2575-minimum-cuts-to-divide-a-circle.c
@@ -0,0 +1,12 @@
+int numberOfCuts(int n) {
+    int result;
+    if(n == 1){
+        result = 0;
+    }
+    else if(n%2 == 0){
+        result = n/2;
+    } else if(n%2 == 1){
+        result = n;
+    }
+    return result;
+}
