feaab28d2e15e5c46d74b8c849594cb5f650ad95
yhqz01 <157672792+YHQZ1@users.noreply.github.com>
2025-04-08 12:15:05 +0530

    Time: 1 ms (98.39%), Space: 44.3 MB (73.87%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,29 +1,12 @@
 class Solution {
     public int minimumOperations(int[] nums) {
         int n = nums.length;
-        int ops = 0;
-        List<Integer> list = new ArrayList<>();
-        for (int num : nums) list.add(num);
-        
-        while (true) {
-            Set<Integer> seen = new HashSet<>();
-            boolean hasDuplicate = false;
-            
-            for (int num : list) {
-                if (seen.contains(num)) {
-                    hasDuplicate = true;
-                    break;
-                }
-                seen.add(num);
-            }
-
-            if (!hasDuplicate) break; 
-            
-            int toRemove = Math.min(3, list.size());
-            list.subList(0, toRemove).clear();
-            ops++;
+        boolean[] seen = new boolean[101];
+        for(int i = n - 1 ; i >= 0 ; i--){
+            if(seen[nums[i]])
+                return i / 3 + 1;
+            else seen[nums[i]] = true;
         }
-
-        return ops;
+        return 0;
     }
 }
