b1e940d3fe587aa86b9b30ed82f13b317547650a
yhqz01 <157672792+YHQZ1@users.noreply.github.com>
2025-06-19 16:52:13 +0530

    Time: 16 ms (80.61%), Space: 45.1 MB (64.25%) - LeetHub

--- 2486-most-frequent-even-element/2486-most-frequent-even-element.java
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.java
@@ -1,25 +1,24 @@
 class Solution {
     public int mostFrequentEven(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int maxFreq = 0;
-        int result = -1;
+        int max = -1;
+        int maxFreq = -1;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) {  // Only count even numbers
-                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] % 2 == 0){
+                if(map.containsKey(nums[i])){
+                    map.put(nums[i], map.get(nums[i]) + 1);
+                } else {
+                    map.put(nums[i], 1);
+                }
+                int freq = map.get(nums[i]);
+
+                if(freq > maxFreq || freq == maxFreq && nums[i] < max){
+                    max = nums[i];
+                    maxFreq = freq;
+                }
             }
         }
-
-        for (int key : map.keySet()) {
-            int freq = map.get(key);
-            if (freq > maxFreq) {
-                maxFreq = freq;
-                result = key;
-            } else if (freq == maxFreq && key < result) {
-                result = key;
-            }
-        }
-
-        return result;
+        return max;
     }
 }
