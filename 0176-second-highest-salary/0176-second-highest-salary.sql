60cd15395ced8ac7aca7fe88a06f9f8ac53a79c1
yhqz01 <157672792+YHQZ1@users.noreply.github.com>
2025-06-20 23:16:54 +0530

    Time: 262 ms (82.69%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,8 +1,3 @@
-SELECT 
-    IFNULL(
-        (SELECT DISTINCT salary 
-         FROM Employee 
-         ORDER BY salary DESC 
-         LIMIT 1 OFFSET 1), 
-        NULL
-    ) AS SecondHighestSalary;
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee
+WHERE Salary < (SELECT MAX(Salary) FROM Employee)
