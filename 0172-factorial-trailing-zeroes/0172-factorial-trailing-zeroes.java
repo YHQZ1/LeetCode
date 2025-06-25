81f735893f063645d053da66ca6ad8f6e692641e
yhqz01 <157672792+YHQZ1@users.noreply.github.com>
2025-06-25 23:33:53 +0530

    Time: 0 ms (100%), Space: 40.9 MB (36.24%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,14 +1,5 @@
 class Solution {
-    public int factorial(int num) {
-        if (num == 0)
-            return 1;
-        if (num == 1)
-            return 1;
-        return num * factorial(num - 1);
-    }
-
     public int trailingZeroes(int n) {
-        int fact = factorial(n);
         int count = 0;
         while (n > 0) {
             n = n / 5;
