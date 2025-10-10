4f288807289f278def5f3c084fc08cf2d0147805
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-10-10 12:47:46 +0530

    Time: 2 ms (97.93%), Space: 59.5 MB (84.14%) - LeetHub

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.java
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.java
@@ -1,15 +1,13 @@
 class Solution {
     public int maximumEnergy(int[] energy, int k) {
-        int n = energy.length;
         int max = Integer.MIN_VALUE;
 
-        for (int i = n - 1; i >= 0; i--) {
-            if (i + k < n) {
+        for (int i = energy.length - 1; i >= 0; i--) {
+            if (i + k < energy.length) {
                 energy[i] = energy[i] + energy[i + k];
             }
             max = Math.max(max, energy[i]);
         }
-
         return max;
     }
 }
