ea9e1915c5091175ad20a9d9b6c1f39ec90b6bd8
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-10-14 20:06:15 +0530

    Time: 1 ms (99.7%), Space: 45.1 MB (74.24%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,23 +1,15 @@
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        
-        // Initialize with large value
-        for (int i = 1; i < n; i++) {
-            dp[i] = Integer.MAX_VALUE;
-        }
-        
-        dp[0] = 0;
-        
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (j + nums[j] >= i && dp[j] != Integer.MAX_VALUE) {
-                    dp[i] = Math.min(dp[i], dp[j] + 1);
-                }
+        int count = 0, last = 0, curr = 0;
+
+        for(int i=0; i<nums.length-1; i++){
+            last = Math.max(last, i + nums[i]);
+
+            if(i == curr){
+                count++;
+                curr = last;
             }
         }
-        
-        return dp[n - 1];
+        return count;
     }
 }
