1e7cabb74874c134e93ce96636cac16441453063
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-10-29 17:46:52 +0530

    Time: 0 ms (100%), Space: 40.8 MB (46.73%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.java
@@ -1,12 +1,7 @@
 class Solution {
     public int smallestNumber(int n) {
-        int[] binary = {0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
-        int count = 0;
-
-        for(int i=0; i<binary.length; i++){
-            if(n < binary[i]) break;
-            else count++;
-        }
-        return binary[count] - 1;
+        int num = n;
+        while((num&(num+1)) != 0) num++;
+        return num;
     }
 }
