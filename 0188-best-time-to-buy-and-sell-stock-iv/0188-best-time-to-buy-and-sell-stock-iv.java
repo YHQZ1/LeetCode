293f377023995d1c79a1ea5ce89d22c48873a1b9
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-11-22 00:08:58 +0530

    Time: 1 ms (99.77%), Space: 43.4 MB (63.26%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -3,16 +3,6 @@ class Solution {
         int n = prices.length;
         if (n <= 1 || k == 0) return 0;
 
-        if (k >= n / 2) {
-            int profit = 0;
-            for (int i = 1; i < n; i++) {
-                if (prices[i] > prices[i - 1]) {
-                    profit += prices[i] - prices[i - 1];
-                }
-            }
-            return profit;
-        }
-
         int[] buy = new int[k + 1];
         int[] sell = new int[k + 1];
 
