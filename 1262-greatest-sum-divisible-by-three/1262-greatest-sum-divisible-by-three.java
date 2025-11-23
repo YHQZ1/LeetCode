5fe42d0e298760260cbfc6e810574e3b7ff07f0f
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-11-23 19:46:16 +0530

    Time: 14 ms (53.73%), Space: 49.3 MB (47.52%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -1,37 +1,21 @@
 class Solution {
     public int maxSumDivThree(int[] nums) {
-        int total = 0;
-
-        int rem1a = Integer.MAX_VALUE, rem1b = Integer.MAX_VALUE;
-        int rem2a = Integer.MAX_VALUE, rem2b = Integer.MAX_VALUE;
+        int[] dp = {0, Integer.MIN_VALUE, Integer.MIN_VALUE};
 
         for (int num : nums) {
-            total += num;
+            int[] next = dp.clone();
             int rem = num % 3;
 
-            if (rem == 1) {
-                if (num < rem1a) { rem1b = rem1a; rem1a = num; }
-                else if (num < rem1b) { rem1b = num; }
-            } else if (rem == 2) {
-                if (num < rem2a) { rem2b = rem2a; rem2a = num; }
-                else if (num < rem2b) { rem2b = num; }
+            for (int r = 0; r < 3; r++) {
+                if (dp[r] != Integer.MIN_VALUE) {
+                    int newRem = (r + rem) % 3;
+                    next[newRem] = Math.max(next[newRem], dp[r] + num);
+                }
             }
+
+            dp = next;
         }
 
-        int rem = total % 3;
-
-        if (rem == 1) {
-            int op1 = rem1a < Integer.MAX_VALUE ? total - rem1a : Integer.MIN_VALUE;
-            int op2 = rem2b < Integer.MAX_VALUE ? total - (rem2a + rem2b) : Integer.MIN_VALUE;
-            return Math.max(op1, op2);
-        }
-
-        if (rem == 2) {
-            int op1 = rem2a < Integer.MAX_VALUE ? total - rem2a : Integer.MIN_VALUE;
-            int op2 = rem1b < Integer.MAX_VALUE ? total - (rem1a + rem1b) : Integer.MIN_VALUE;
-            return Math.max(op1, op2);
-        }
-
-        return total;
+        return dp[0];
     }
 }
