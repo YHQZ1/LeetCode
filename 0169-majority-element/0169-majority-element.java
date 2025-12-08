b41e618dc7fe692c0484740d4bd7bbf85059df37
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-12-09 00:19:39 +0530

    Time: 16 ms (19.22%), Space: 52.1 MB (86.08%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,38 +1,21 @@
-public class Solution {
-
+class Solution {
     public int majorityElement(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
         int res = -1;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
                 map.put(nums[i], map.get(nums[i]) + 1);
             } else {
                 map.put(nums[i], 1);
             }
         }
 
-        for (int key : map.keySet()) {
-            if (map.get(key) > nums.length / 2) {
+        for(int key : map.keySet()){
+            if(map.get(key) > nums.length/2){
                 res = key;
             }
         }
         return res;
-    }
-
-    public static void main(String[] args) {
-
-        // Your shutdown hook
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter fw = new FileWriter("display_runtime.txt")) {
-                fw.write("000");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }));
-
-        // If you want to test majorityElement:
-        // Solution s = new Solution();
-        // System.out.println(s.majorityElement(new int[]{2,2,1,1,1,2,2}));
     }
 }
