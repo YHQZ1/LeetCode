fd3f821d463b452b8fcde226b7b71594ce6a4cfa
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-12-13 00:11:27 +0530

    Time: 1 ms (99.17%), Space: 47.2 MB (44.22%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,13 +1,12 @@
 class Solution {
     public int jump(int[] nums) {
-        int count = 0, last = 0, curr = 0;
+        int count = 0, max = 0, curr = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            max = Math.max(max, i + nums[i]);
 
-        for(int i=0; i<nums.length-1; i++){
-            last = Math.max(last, i + nums[i]);
-
-            if(i == curr){
+            if (i == curr) {
                 count++;
-                curr = last;
+                curr = max;
             }
         }
         return count;
