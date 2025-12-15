b492b780c98201242953e019ba4475b30fe8ab45
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-12-16 00:49:37 +0530

    Time: 3 ms (87.16%), Space: 47.6 MB (31.39%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] candies = new int[ratings.length];
+        int count = 0;
+
+        for(int i=0; i<candies.length; i++){
+            candies[i] = 1;
+        }
+
+        for(int i=1; i<candies.length; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+
+        for(int i=candies.length-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = Math.max(candies[i], candies[i+1] + 1);
+            }
+        }
+        for(int c : candies){
+            count += c;
+        }
+        return count;
+    }
+}
