08fcae561b138a38e8ff59a946c53f50f297300b
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-12-25 15:38:55 +0530

    Time: 2 ms (14.61%), Space: 47.5 MB (9.75%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        Set<Integer> flagRows = new HashSet<>();
+        Set<Integer> flagCols = new HashSet<>();
+
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    flagRows.add(i);
+                    flagCols.add(j);
+                }
+            }
+        }
+
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(flagRows.contains(i) || flagCols.contains(j)){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
