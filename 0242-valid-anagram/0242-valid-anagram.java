9cc14005b8dcac5480a448115e2c7aa6bdff15b1
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-12-27 00:50:12 +0530

    Time: 16 ms (22.38%), Space: 46.9 MB (7.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,29 +1,32 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
 
-        if(s.length() != t.length()) return false;
-
-        HashMap<Character, Integer> T = new HashMap<>();
         HashMap<Character, Integer> S = new HashMap<>();
+        HashMap<Character, Integer> T = new HashMap<>();
 
-        for(int i=0; i<s.length(); i++){
-            char insert = s.charAt(i);
-            if(S.containsKey(insert)){
-                S.put(insert, S.get(insert) + 1);
-            } else {
-                S.put(insert, 1);
-            }
+        for (int i = 0; i < s.length(); i++) {
+            S.put(s.charAt(i), S.getOrDefault(s.charAt(i), 0) + 1);
         }
 
-        for(int i=0; i<t.length(); i++){
-            char insert = t.charAt(i);
-            if(T.containsKey(insert)){
-                T.put(insert, T.get(insert) + 1);
-            } else {
-                T.put(insert, 1);
-            }
+        for (int i = 0; i < t.length(); i++) {
+            T.put(t.charAt(i), T.getOrDefault(t.charAt(i), 0) + 1);
         }
 
+        // for (int i = 0; i < t.length(); i++) {
+        //     char curr = t.charAt(i);
+        //     if (S.containsKey(curr)) {
+        //         int count = S.get(curr) - 1;
+        //         if (count == 0) {
+        //             S.remove(curr);
+        //         } else {
+        //             S.put(curr, count);
+        //         }
+        //     } else {
+        //         return false;
+        //     }
+        // }
         return S.equals(T);
     }
 }
