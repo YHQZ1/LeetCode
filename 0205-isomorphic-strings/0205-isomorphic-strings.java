681cebf2b086c0dd5d454500f91ebe1d865a0b1c
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-12-26 21:39:58 +0530

    Time: 13 ms (49.4%), Space: 44.7 MB (5.47%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,27 +1,35 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> map = new HashMap<>();
-        HashSet<Character> mappedValues = new HashSet<>(); 
-
-        if (s.length() != t.length()) return false;
+        HashMap<Character, Character> map1 = new HashMap<>();
+        HashMap<Character, Character> map2 = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            char original = s.charAt(i);
-            char replacement = t.charAt(i);
+            char key = s.charAt(i);
+            char value = t.charAt(i);
 
-            if (!map.containsKey(original)) {
-                if (!mappedValues.contains(replacement)) {
-                    map.put(original, replacement);
-                    mappedValues.add(replacement);
-                } else {
-                    return false; 
-                }
-            } else {
-                char mapped = map.get(original);
-                if (mapped != replacement) return false;
-            }
+            map1.put(key, value);
         }
 
-        return true;
+        for (int i = 0; i < t.length(); i++) {
+            char key = t.charAt(i);
+            char value = s.charAt(i);
+
+            map2.put(key, value);
+        }
+
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char curr = map1.get(s.charAt(i));
+            sb1.append(curr);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char curr = map2.get(t.charAt(i));
+            sb2.append(curr);
+        }
+
+        return (sb1.toString().equals(t) && sb2.toString().equals(s));
     }
 }
