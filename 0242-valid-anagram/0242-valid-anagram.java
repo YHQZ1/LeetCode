402a4f2f1edf263dd563d3b3b9d95a3704a3d336
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-12-27 00:51:45 +0530

    Time: 26 ms (6.14%), Space: 44.9 MB (49.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,30 +3,17 @@ class Solution {
         if (s.length() != t.length())
             return false;
 
-        HashMap<Character, Integer> S = new HashMap<>();
-        HashMap<Character, Integer> T = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            S.put(s.charAt(i), S.getOrDefault(s.charAt(i), 0) + 1);
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            T.put(t.charAt(i), T.getOrDefault(t.charAt(i), 0) + 1);
+        for (int val : map.values()) {
+            if (val != 0)
+                return false;
         }
-
-        // for (int i = 0; i < t.length(); i++) {
-        //     char curr = t.charAt(i);
-        //     if (S.containsKey(curr)) {
-        //         int count = S.get(curr) - 1;
-        //         if (count == 0) {
-        //             S.remove(curr);
-        //         } else {
-        //             S.put(curr, count);
-        //         }
-        //     } else {
-        //         return false;
-        //     }
-        // }
-        return S.equals(T);
+        return true;
     }
 }
