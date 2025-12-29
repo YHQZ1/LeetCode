b3e75e201a4bfbff25760908ab02eb8367857cdc
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-12-29 23:45:00 +0530

    Time: 1 ms (97.58%), Space: 47.2 MB (30.87%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,46 +1,27 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        if (intervals.length == 0) {
-            return new int[][] { newInterval };
+        List<int[]> res = new ArrayList<>();
+        int i = 0;
+        int n = intervals.length;
+
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i]);
+            i++;
         }
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        List<int[]> list = new ArrayList<>(Arrays.asList(intervals));
-        boolean inserted = false;
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i)[0] > newInterval[0]) {
-                list.add(i, new int[] { newInterval[0], newInterval[1] });
-                inserted = true;
-                break;
-            }
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
         }
 
-        if (!inserted) {
-            list.add(new int[] { newInterval[0], newInterval[1] });
+        res.add(newInterval);
+
+        while (i < n) {
+            res.add(intervals[i]);
+            i++;
         }
 
-        int[][] updated = list.toArray(new int[list.size()][]);
-
-        List<int[]> merged = new ArrayList<>();
-
-        int start = updated[0][0];
-        int end = updated[0][1];
-
-        for (int i = 1; i < updated.length; i++) {
-            int currStart = updated[i][0];
-            int currEnd = updated[i][1];
-
-            if (currStart <= end) {
-                end = Math.max(end, currEnd);
-            } else {
-                merged.add(new int[] { start, end });
-                start = currStart;
-                end = currEnd;
-            }
-        }
-        merged.add(new int[] { start, end });
-        return merged.toArray(new int[merged.size()][]);
-
+        return res.toArray(new int[res.size()][]);
     }
 }
