f1ed245d7aeb31e254c722066db0c39385221b82
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2025-12-29 12:40:05 +0530

    Time: 134 ms (15.85%), Space: 46.3 MB (95.83%) - LeetHub

--- 0454-4sum-ii/0454-4sum-ii.java
+++ 0454-4sum-ii/0454-4sum-ii.java
@@ -5,11 +5,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int sum = nums1[i] + nums2[j];
-                if (!map.containsKey(sum)) {
-                    map.put(sum, 1);
-                } else {
-                    map.put(sum, map.getOrDefault(sum, 0) + 1);
-                }
+                map.put(sum, map.getOrDefault(sum, 0) + 1);
             }
         }
 
@@ -18,7 +14,7 @@ class Solution {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int sum = nums3[i] + nums4[j];
-                int neg = sum * -1;
+                int neg = -sum;
                 if (map.containsKey(neg))
                     count += map.get(neg);
             }
