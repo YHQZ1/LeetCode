64b591cb2b9dd62cfafca4e83b25bb5e87b0662e
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-01-15 23:00:00 +0530

    Time: 4 ms (95.24%), Space: 43.6 MB (62.06%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -1,29 +1,17 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        Map<Character, Integer> freq = new HashMap<>();
         List<Integer> ans = new ArrayList<>();
+        int[] last = new int[26];
 
-        // Step 1: build frequency map
-        for (char c : s.toCharArray()) {
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        for (int i = 0; i < s.length(); i++) {
+            last[s.charAt(i) - 'a'] = i;
         }
 
-        Set<Character> active = new HashSet<>();
-        int start = 0;
+        int maxReach = 0, start = 0;
 
-        // Step 2: process string
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-
-            active.add(c);
-            freq.put(c, freq.get(c) - 1);
-
-            if (freq.get(c) == 0) {
-                active.remove(c);
-            }
-
-            // partition condition
-            if (active.isEmpty()) {
+            maxReach = Math.max(maxReach, last[s.charAt(i) - 'a']);
+            if (i == maxReach) {
                 ans.add(i - start + 1);
                 start = i + 1;
             }
