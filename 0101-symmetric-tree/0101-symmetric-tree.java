5252f474a269c5e718c699f125abfafcaa19d5c2
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-01-25 17:11:15 +0530

    Time: 0 ms (100%), Space: 43.7 MB (24.4%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,23 +14,19 @@
  * }
  */
 class Solution {
-    public boolean isMirror(TreeNode p, TreeNode q){
-        if(p == null && q == null){
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
             return true;
-        }
-
-        if(p == null || q == null){
+        if (p == null || q == null)
             return false;
-        }
-
-        return p.val == q.val && isMirror(p.left, q.right) && isMirror(p.right, q.left);
+        if (p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.right) && isSameTree(p.right, q.left);
     }
 
     public boolean isSymmetric(TreeNode root) {
-        if(root == null){
+        if (root == null)
             return true;
-        }
-
-        return isMirror(root.left, root.right);
+        return isSameTree(root.left, root.right);
     }
 }
