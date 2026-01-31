33a54fc3d00aa48abad2723afa338cb6f6a73ee8
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-01-31 19:30:34 +0530

    Time: 0 ms (100%), Space: 49.7 MB (9.88%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -15,7 +15,8 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        if (root == null) return 0;
+        if (root == null)
+            return 0;
         return 1 + countNodes(root.left) + countNodes(root.right);
     }
 }
