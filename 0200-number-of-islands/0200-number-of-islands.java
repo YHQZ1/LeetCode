6b56d58293bde2581d4285f3339ac1b9b5279d9c
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-02-02 22:27:53 +0530

    Time: 3 ms (87.48%), Space: 52.4 MB (54.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -7,10 +7,10 @@ class Solution {
 
         grid[i][j] = '0';
 
-        dfs(grid, i - 1, j);
         dfs(grid, i + 1, j);
-        dfs(grid, i, j - 1);
+        dfs(grid, i - 1, j);
         dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
 
     }
 
