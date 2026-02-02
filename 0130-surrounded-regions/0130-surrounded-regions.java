cef8a586e77f9abed4c162174189318017b1250e
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-02-02 23:53:46 +0530

    Time: 2 ms (82.6%), Space: 48 MB (18.88%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -19,29 +19,28 @@ class Solution {
     }
 
     public void solve(char[][] board) {
-        int n = board.length, m = board[0].length;
+        int n = board.length;
+        int m = board[0].length;
+
         boolean[][] visited = new boolean[n][m];
-        for (int j = 0; j < m; j++) {
-            if (!visited[0][j] && board[0][j] == 'O') {
-                dfs(board, visited, 0, j);
-            }
-            if (!visited[n - 1][j] && board[n - 1][j] == 'O') {
-                dfs(board, visited, n - 1, j);
-            }
-        }
 
         for (int i = 0; i < n; i++) {
-            if (!visited[i][0] && board[i][0] == 'O') {
+            if (!visited[i][0] && board[i][0] == 'O')
                 dfs(board, visited, i, 0);
-            }
-            if (!visited[i][m - 1] && board[i][m - 1] == 'O') {
+            if (!visited[i][m - 1] && board[i][m - 1] == 'O')
                 dfs(board, visited, i, m - 1);
-            }
+        }
+
+        for (int j = 0; j < m; j++) {
+            if (!visited[0][j] && board[0][j] == 'O')
+                dfs(board, visited, 0, j);
+            if (!visited[n - 1][j] && board[n - 1][j] == 'O')
+                dfs(board, visited, n - 1, j);
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (!visited[i][j] && board[i][j] == 'O')
+                if (board[i][j] == 'O' && !visited[i][j])
                     board[i][j] = 'X';
             }
         }
