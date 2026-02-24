cb62db7abcba9409fc596941751ce3d38aa5202f
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-02-24 15:38:21 +0530

    Time: 12 ms (41.99%), Space: 46.8 MB (29.62%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,35 +1,34 @@
-import java.util.Stack;
-
 class Solution {
     public int calculate(String s) {
         int result = 0;
         int curr = 0;
         int sign = 1;
+
         Stack<Integer> stack = new Stack<>();
 
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+            char c = s.charAt(i);
 
-            if (Character.isDigit(ch)) {
-                curr = curr * 10 + (ch - '0');
-            } else if (ch == '+') {
+            if (Character.isDigit(c))
+                curr = curr * 10 + (c - '0');
+            else if (c == '+') {
                 result = result + curr * sign;
                 curr = 0;
                 sign = 1;
-            } else if (ch == '-') {
+            } else if (c == '-') {
                 result = result + curr * sign;
                 curr = 0;
                 sign = -1;
-            } else if (ch == '(') {
+            } else if (c == '(') {
                 stack.push(result);
                 stack.push(sign);
                 result = 0;
                 sign = 1;
-            } else if (ch == ')') {
+            } else if (c == ')') {
                 result = result + curr * sign;
                 curr = 0;
-                result = result * stack.pop();
-                result = result + stack.pop();
+                result *= stack.pop();
+                result += stack.pop();
             }
         }
         return result + curr * sign;
