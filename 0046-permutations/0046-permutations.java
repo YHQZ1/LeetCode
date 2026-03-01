a854ee5402c504cb1b7012af1e3924142ec070e6
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-03-01 13:06:21 +0530

    Time: 0 ms (100%), Space: 45.6 MB (46.89%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,6 +1,12 @@
 class Solution {
-    public void getPerms(int[] nums, int idx, List<List<Integer>> result) {
-        if (idx == nums.length) {
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void getPermutations(int[] nums, int index, List<List<Integer>> result) {
+        if (index == nums.length) {
             List<Integer> perm = new ArrayList<>();
             for (int n : nums)
                 perm.add(n);
@@ -8,22 +14,16 @@ class Solution {
             return;
         }
 
-        for (int i = idx; i < nums.length; i++) {
-            swap(nums, i, idx);
-            getPerms(nums, idx + 1, result);
-            swap(nums, i, idx);
+        for (int i = index; i < nums.length; i++) {
+            swap(nums, i, index);
+            getPermutations(nums, index + 1, result);
+            swap(nums, i, index);
         }
-    }
-
-    private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
     }
 
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        getPerms(nums, 0, result);
+        getPermutations(nums, 0, result);
         return result;
     }
 }
