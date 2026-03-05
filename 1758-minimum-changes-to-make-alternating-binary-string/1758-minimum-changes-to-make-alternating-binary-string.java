d0551b387b18c280e27accbb8b7b172f894ab826
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-03-05 10:01:15 +0530

    Time: 5 ms (53.53%), Space: 43.4 MB (95.87%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,16 +1,12 @@
 class Solution {
     public int minOperations(String s) {
-        int diff0 = 0, diff1 = 0;
+        int mismatch = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            char expected0 = (i % 2 == 0) ? '0' : '1';
-            char expected1 = (i % 2 == 0) ? '1' : '0';
-
-            if (s.charAt(i) != expected0)
-                diff0++;
-            if (s.charAt(i) != expected1)
-                diff1++;
+            if ((s.charAt(i) - '0') != (i % 2))
+                mismatch++;
         }
-        return Math.min(diff0, diff1);
+
+        return Math.min(mismatch, s.length() - mismatch);
     }
 }
