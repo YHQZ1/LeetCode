4393ec2a0c9dfd9ee8de269b4ea83c40c4ee09aa
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-03-11 23:46:15 +0530

    Time: 0 ms (100%), Space: 42.2 MB (50.09%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        int bitLength = Integer.toBinaryString(n).length();
+        int mask = (1 << bitLength) - 1;
+
+        int flipped = n ^ mask;
+        return flipped;
+    }
+}
