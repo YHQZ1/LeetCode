b19ab5e5bec5f3d897f002f14ec73ab6d40f2265
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-03-30 00:54:23 +0530

    Time: 1 ms (100%), Space: 44.5 MB (36.73%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,23 +1,10 @@
 class Solution {
-    public void swapChars(StringBuilder sb, int i, int j) {
-        char temp = sb.charAt(i);
-        sb.setCharAt(i, sb.charAt(j));
-        sb.setCharAt(j, temp);
+    public boolean canBeEqual(String s1, String s2) {
+        return match(s1.charAt(0), s1.charAt(2), s2.charAt(0), s2.charAt(2)) &&
+                match(s1.charAt(1), s1.charAt(3), s2.charAt(1), s2.charAt(3));
     }
 
-    public boolean canBeEqual(String s1, String s2) {
-        if (s1.equals(s2))
-            return true;
-            
-        StringBuilder swap1 = new StringBuilder(s1);
-        StringBuilder swap2 = new StringBuilder(s1);
-        StringBuilder swap3 = new StringBuilder(s1);
-
-        swapChars(swap1, 0, 2);
-        swapChars(swap2, 1, 3);
-        swapChars(swap3, 0, 2);
-        swapChars(swap3, 1, 3);
-
-        return s2.equals(swap1.toString()) || s2.equals(swap2.toString()) || s2.equals(swap3.toString());
+    private boolean match(char a1, char a2, char b1, char b2) {
+        return (a1 == b1 && a2 == b2) || (a1 == b2 && a2 == b1);
     }
 }
