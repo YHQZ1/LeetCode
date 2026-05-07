7788c60d85dd5b13bf3044df41c082a5f4c31e78
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-05-07 20:40:36 +0530

    Time: 5 ms (87.1%), Space: 197.7 MB (85.48%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.java
+++ 3660-jump-game-ix/3660-jump-game-ix.java
@@ -1,40 +1,16 @@
 class Solution {
-    record Item(int value, int index) {
-    }
-
     public int[] maxValue(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[n];
-        Item[] prevMax = new Item[n];
+        int n = nums.length, min = Integer.MAX_VALUE;
+        int[] max = new int[n], ans = new int[n];
+        max[0] = nums[0];
+        for (int i = 1; i < n; i++)
+            max[i] = Math.max(max[i - 1], nums[i]);
 
-        Item prev = new Item(Integer.MIN_VALUE, -1);
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > prev.value()) {
-                prev = new Item(nums[i], i);
-            }
-            prevMax[i] = prev;
+        for (int i = n - 2; i >= 0; i--) {
+            min = Math.min(min, nums[i + 1]);
+            if (min < max[i])
+                max[i] = max[i + 1];
         }
-
-        process(n - 1, Integer.MAX_VALUE, 0, prevMax, ans, nums);
-        return ans;
-    }
-
-    private void process(int r, int rightMin, int rightMax, Item[] prevMax, int[] ans, int[] nums) {
-        int pMax = prevMax[r].value();
-        int pivotIndex = prevMax[r].index();
-
-        int currMax = pMax <= rightMin ? pMax : rightMax;
-
-        int nextRightMin = Math.min(pMax, rightMin);
-        for (int i = pivotIndex; i <= r; i++) {
-            ans[i] = currMax;
-            nextRightMin = Math.min(nextRightMin, nums[i]);
-        }
-
-        if (pivotIndex == 0) {
-            return;
-        }
-
-        process(pivotIndex - 1, nextRightMin, currMax, prevMax, ans, nums);
+        return max;
     }
 }
