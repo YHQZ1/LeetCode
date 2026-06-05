2e11f359b98697755db6d2f9389f050437275279
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-06-05 14:53:28 +0530

    Time: 1 ms (95.78%), Space: 46.7 MB (59.89%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -14,15 +14,14 @@
  * }
  */
 class Solution {
-    public void helper(TreeNode root, List<List<Integer>> list, int depth) {
+    private void helper(TreeNode root, List<List<Integer>> list, int depth) {
         if (root == null)
             return;
-
-        if (list.size() <= depth)
+        if (list.size() <= depth) {
             list.add(new ArrayList<>());
+        }
 
         list.get(depth).add(root.val);
-
         helper(root.left, list, depth + 1);
         helper(root.right, list, depth + 1);
     }
