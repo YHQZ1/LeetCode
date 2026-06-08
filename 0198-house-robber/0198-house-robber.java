806353af291366a1e1518a63d3027e6910635cdd
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-06-08 09:48:20 +0530

    Time: 0 ms (100%), Space: 43 MB (17.93%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,9 +3,6 @@ class Solution {
         if (nums.length == 1)
             return nums[0];
 
-        if (nums.length == 2)
-            return Math.max(nums[0], nums[1]);
-
         int[] dp = new int[nums.length];
 
         dp[0] = nums[0];
