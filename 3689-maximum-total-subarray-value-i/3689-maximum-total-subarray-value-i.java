913ad6c35a60e0cb7847a8f7d09e5e64cd1176c7
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-06-10 11:23:32 +0530

    Time: 1 ms (100%), Space: 62.5 MB (21.32%) - LeetHub

--- 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
+++ 3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.java
@@ -4,8 +4,8 @@ class Solution {
         long min = Long.MAX_VALUE;
 
         for(int i=0; i<nums.length; i++){
-            if(nums[i] > max) max = Math.max(nums[i], max);
-            if(nums[i] < min) min = Math.min(nums[i], min);
+            max = Math.max(nums[i], max);
+            min = Math.min(nums[i], min);
         }
 
         return k * (max - min);
