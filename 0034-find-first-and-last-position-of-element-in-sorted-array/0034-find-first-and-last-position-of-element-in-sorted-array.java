57686a2c69d45d1f9648c5718ea2bb568fb6bcb2
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-06-24 17:42:08 +0530

    Time: 0 ms (100%), Space: 48.4 MB (13.49%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,28 +1,40 @@
 class Solution {
-    public int searchIndex(int[] nums, int t, boolean find){
-        int l = 0, r = nums.length-1, i = -1;
-        while(l <= r){
-            int m = l + (r-l)/2;
-            if(nums[m] == t){
-                i = m;
-                if (find){
-                    r = m - 1;
-                } else {
-                    l = m + 1;
-                }
-            } else if (nums[m] < t){
-                l = m + 1;
+    public int[] searchRange(int[] nums, int target) {
+        int[] indices = new int[2];
+        indices[0] = Integer.MAX_VALUE;
+        indices[1] = Integer.MIN_VALUE;
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target && mid < indices[0]) {
+                indices[0] = mid;
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
             } else {
-                r = m - 1;
+                right = mid - 1;
             }
         }
-        return i;
-    }
 
-    public int[] searchRange(int[] nums, int target) {
-        int fi, li;
-        fi = searchIndex(nums, target, true);
-        li = searchIndex(nums, target, false);
-        return new int[]{fi, li}; 
+        left = 0;
+        right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target && mid > indices[1]) {
+                indices[1] = mid;
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        if (indices[0] == Integer.MAX_VALUE && indices[1] == Integer.MIN_VALUE) {
+            return new int[] { -1, -1 };
+        }
+
+        return indices;
     }
 }
