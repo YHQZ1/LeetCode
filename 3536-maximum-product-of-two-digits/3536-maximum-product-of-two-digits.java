2f93eee13da1c5d806ed3e63902265a3f9dcd230
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-07-26 00:17:21 +0530

    Time: 1 ms (100%), Space: 42.6 MB (74.48%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -4,10 +4,10 @@ class Solution {
 
         while (n > 0) {
             int rem = n % 10;
-            if (rem >= max1) {
+            if (rem > max1) {
                 max2 = max1;
                 max1 = rem;
-            } else if (rem >= max2 && rem < max1) {
+            } else if (rem > max2) {
                 max2 = rem;
             }
 
