6f00319e5b83e026f348e8ee03dd1db6a309694c
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-08-18 14:03:07 +0530

    Time: 2 ms (72.63%), Space: 45 MB (62.11%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,12 +1,13 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int res = -1;
         int n = nums.length;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int num : nums) {
+        int res = -1;
+
+        for (int num : nums)
             map.put(num, map.getOrDefault(num, 0) + 1);
-        }
 
         if (k == 1) {
             for (int num : nums) {
@@ -14,20 +15,15 @@ class Solution {
                     res = Math.max(res, num);
             }
         } else if (k == n) {
-            for (int num : nums) {
+            for (int num : nums)
                 res = Math.max(res, num);
-            }
+
         } else if (1 < k && k < n) {
-            int first = nums[0];
-            int last = nums[n - 1];
-            if (map.get(first) == 1 && map.get(last) == 1)
-                return Math.max(first, last);
-            else if (map.get(first) > 1 && map.get(last) == 1)
-                return last;
-            else if (map.get(first) == 1 && map.get(last) > 1)
-                return first;
-            else if (map.get(first) > 1 && map.get(last) > 1)
-                return -1;
+            if (map.get(nums[0]) == 1)
+                res = Math.max(res, nums[0]);
+
+            if (map.get(nums[n - 1]) == 1)
+                res = Math.max(res, nums[n - 1]);
         }
         return res;
     }
