4c3c06529e488e34a4ddcdaa88fbc483f7d0b812
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-09-07 14:48:58 +0530

    Time: 6 ms (43.62%), Space: 42.9 MB (96.1%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int distinctSubseqII(String s) {
+        int MOD = 1_000_000_007;
+        long[] dp = new long[26];
+
+        for (char c : s.toCharArray()) {
+            int index = c - 'a';
+
+            long total = 1;
+
+            for (long count : dp) {
+                total = (total + count) % MOD;
+            }
+
+            dp[index] = total;
+        }
+
+        long answer = 0;
+
+        for (long count : dp) {
+            answer = (answer + count) % MOD;
+        }
+
+        return (int) answer;
+    }
+}
